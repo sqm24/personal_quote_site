cbbf59ad2a0eab3012cbc8f0c281c3652a07e200
sam <samuelharrell24@gmail.com>
2025-01-08 23:54:49 -0500

    basic sqlite3 instance. need to add more and create API with it. spent most of tonight learning and reading up on it

---
+++ README.md
@@ -0,0 +1,1 @@
+im gonna create a postgresql db to host the actual data. in meantime i wanna tryt and write it so i can drop in the PSQL once I'm done
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def main():
     return render_template("index.html")
 
 if __name__ == '__main__':
